2b56cd99763a23b2f0cc8c52423182874fb6aa85
Giovanna Delfim <gi-martinez@hotmail.com>
2020-11-20 14:52:19 -0300

    adicionando ao git

---
+++ Atividades/Atividade04/Atividade 4 - Duelo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SyncVar]
+    public Color color;
+    public Material material;
+    public GameObject bala;
+    public Transform arma;
+    float moveSpeed = 0.1f;
+    float moveRotation = 10;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        CmdChangeColor(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+    }
+
+    public override void OnStartClient()
+    {
+
+    }
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        material.color = color;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (isServer) Debug.Log("Somente no servidor");
+        //if (isClient) Debug.Log("Somente no cliente");
+
+        if (!isLocalPlayer) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed);
+            transform.Rotate(0, Input.GetAxis("Horizontal") * moveRotation, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject tiro = Instantiate(bala, arma.position, transform.rotation);
+            NetworkServer.Spawn(tiro);
+        }
+    }
+
+    [Command]
+    public void CmdChangeColor (Color newColor)  // cliente envia para o servidor
+    {
+       color = newColor;
+       RpcChangeColor (newColor);
+    }
+
+    [ClientRpc]
+    public void RpcChangeColor (Color newColor)  // servidor envia para os clientes
+    {
+       material.color = newColor;
+    }
+}
---
+++ Aulas/Photon/Assets/Scripts/PhotonConnection.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonConnection : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Conectando na nuvem...");
+
+        //Conecta na nuvem Photon 
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.NickName = "Player"; // + Random.Range(0,9999);
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    override public void OnConnectedToMaster()
+    {
+        Debug.Log("Conectado");
+
+        //Entra em uma sala
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom("JOD061", options, TypedLobby.Default);
+    }
+
+    override public void OnJoinedRoom()
+    {
+        Debug.Log("Jogador " + PhotonNetwork.LocalPlayer.NickName + 
+        " entrou na sala " + PhotonNetwork.CurrentRoom.Name + "(" + 
+        PhotonNetwork.CurrentRoom.PlayerCount + ")");
+
+        //PhotonNetwork.LoadLevel("PhotonScene");
+
+        PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-5f, 5f), 0f, 0f),
+        Quaternion.identity, 0);
+    }
+
+    override public void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Deu ruim ao entrar na sala! Motivo: " + message);
+    }
+
+}
